9dd93cd1592b65f40b774b9145e7ef477883ddd4
AdnanSaedC <adnansaed180@gmail.com>
2026-02-06 22:31:08 +0530

    fixed many things

--- auth/src/middleware.ts
+++ auth/src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
+export function middleware(request: NextRequest) {
     const publicPath = request.nextUrl.pathname
     const token = request.cookies.get("token")?.value
 
